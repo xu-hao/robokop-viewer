d15179ae8c2472437c78b329e63dca234793e621
Hao Xu <xuh@cs.unc.edu>
2019-05-17 13:39:58 -0400

    robokop viewer

--- manager/setup.py
+++ manager/setup.py
@@ -5,7 +5,6 @@ import traceback
 import logging
 
 from flask import Flask, Blueprint
-from flask_mail import Mail
 from flask_restful import Api
 from flask_cors import CORS
 from flasgger import Swagger
@@ -18,19 +17,15 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__, static_folder='../pack', template_folder='../templates')
 # Set default static folder to point to parent static folder where all
 # static assets can be stored and linked
-app.config.from_pyfile('robokop_flask_config.py')
-
-mail = Mail(app)
 
 api_blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(api_blueprint)
-app.register_blueprint(api_blueprint)
 
 template = {
     "openapi": "3.0.1",
     "info": {
-        "title": "ROBOKOP Manager",
-        "description": "An API for management of biomedical questions and answers",
+        "title": "ROBOKOP Viewer",
+        "description": "An API for viewing biomedical questions and answers",
         "contact": {
             "name": "NCATS Gamma",
             "email": "patrick@covar.com",
@@ -63,7 +58,7 @@ swagger_config = {
 }
 
 app.config['SWAGGER'] = {
-    'title': 'ROBOKOP Manager API',
+    'title': 'ROBOKOP Viewer API',
     'uiversion': 3
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -11,22 +11,7 @@ import 'react-widgets/dist/css/react-widgets.css';
 
 import 'babel-polyfill'; // For IE Promises
 
-import Landing from './Landing';
-import Help from './Help';
-import TermsofService from './TermsofService';
-import QuestionNew from './QuestionNew';
-import QuestionNewLinear from './QuestionNewLinear';
-import Question from './Question';
-import QuestionList from './QuestionList';
-import Answerset from './Answerset';
-import Activity from './Activity';
-import MultiSearch from './MultiSearch';
 import SimpleViewer from './SimpleViewer';
-import SimpleEnriched from './SimpleEnriched';
-import SimpleSimilarity from './SimpleSimilarity';
-import SimpleExpand from './SimpleExpand';
-import Synonymize from './Synonymize';
-import NewQuestionStore from './stores/newQuestionStore';
 
 // Our actual CSS and other images etc.
 import '../assets/css/style.css';
@@ -57,93 +42,6 @@ const config = {
 
 const robokop = {
   config,
-  landing: () => {
-    ReactDOM.render(
-      <Landing
-        config={config}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
-  help: () => {
-    ReactDOM.render(
-      <Help
-        config={config}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
-  termsofservice: () => {
-    ReactDOM.render(
-      <TermsofService
-        config={config}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
-  questionList: () => {
-    ReactDOM.render(
-      <QuestionList
-        config={config}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
-  questionNew: (id) => {
-    ReactDOM.render(
-      <Provider store={new NewQuestionStore()}>
-        <QuestionNew
-          config={config}
-          initializationId={id}
-        />
-      </Provider>,
-      document.getElementById('reactEntry'),
-    );
-  },
-  questionNewLinear: (id) => {
-    ReactDOM.render(
-      <QuestionNewLinear
-        config={config}
-        initializationId={id}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
-  search: () => {
-    ReactDOM.render(
-      <MultiSearch
-        config={config}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
-  activity: () => {
-    ReactDOM.render(
-      <Activity
-        config={config}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
-  question: (questionId) => {
-    ReactDOM.render(
-      <Question
-        config={config}
-        id={questionId}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
-  answerset: (answersetId, answerId) => {
-    ReactDOM.render(
-      <Answerset
-        config={config}
-        id={answersetId}
-        answerId={answerId}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
   simpleView: (id) => {
     ReactDOM.render(
       <SimpleViewer
@@ -152,41 +50,7 @@ const robokop = {
       />,
       document.getElementById('reactEntry'),
     );
-  },
-  simpleEnriched: () => {
-    ReactDOM.render(
-      <SimpleEnriched
-        config={config}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
-  simpleSimilarity: () => {
-    ReactDOM.render(
-      <SimpleSimilarity
-        config={config}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
-  simpleExpand: () => {
-    ReactDOM.render(
-      <Provider store={new NewQuestionStore()}>
-        <SimpleExpand
-          config={config}
-        />
-      </Provider>,
-      document.getElementById('reactEntry'),
-    );
-  },
-  synonymize: () => {
-    ReactDOM.render(
-      <Synonymize
-        config={config}
-      />,
-      document.getElementById('reactEntry'),
-    );
-  },
+  }
 };
 
 export { robokop, config };
